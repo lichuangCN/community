a25da20f8687dce4767fafbbdc0e2ffb975d3466
lichuangCN <1503031032@qq.com>
2019-09-21 20:20:42 +0800

    重置清空test部分内容

--- src/test/java/vex/muzhi/community/CommunityApplicationTests.java
+++ src/test/java/vex/muzhi/community/CommunityApplicationTests.java
@@ -1,23 +1,11 @@
 package vex.muzhi.community;
 
-import net.coobird.thumbnailator.Thumbnails;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import vex.muzhi.community.enums.NotificationTypeEnum;
-import vex.muzhi.community.enums.NotificationStatusEnum;
-import vex.muzhi.community.mapper.NotificationMapper;
-import vex.muzhi.community.model.Notification;
-
-import java.io.File;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CommunityApplicationTests {
 
-    public void test() {
-
-    }
 }
